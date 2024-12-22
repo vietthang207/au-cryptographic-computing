b3c8b9b0aa3ffb4dda323f02d973b5dc218ffe07
Thang Dinh <16717647+vietthang207@users.noreply.github.com>
2024-12-22 12:00:21 +0100

    using big.Int instead of int64

--- ppml/bigDecimal.go
+++ ppml/bigDecimal.go
@@ -1,76 +1,97 @@
 package ppml
 
-import "math/rand"
+import (
+	"math/big"
+	"math/rand"
+)
 
 const MAX_RAND = 128
 const DEFAULT_SCALAR = 100
 
 type BigDec struct {
-	integral int64
+	integral *big.Int
 	// We use the convention that "scalar" is the inverse of the scalar, so scalar=100 represents a 1/100th scale
 	// - meaning that we divide by the scalar instead of multiplying when the representation is needed
-	scalar int64
+	scalar *big.Int
 }
 
 func CheckScalarDifference(x BigDec, y BigDec) {
-	if x.scalar != y.scalar {
+	if x.scalar.Cmp(y.scalar) != 0 {
 		panic("Different scalars not implemented")
 	}
 }
 
 func Add(x BigDec, y BigDec) BigDec {
 	CheckScalarDifference(x, y)
-	return BigDec{integral: x.integral + y.integral, scalar: x.scalar}
+	resIntegral := new(big.Int)
+	resIntegral.Add(x.integral, y.integral)
+	return BigDec{integral: resIntegral, scalar: x.scalar}
 }
 
-func Sub(x BigDec, y BigDec) BigDec {
-	CheckScalarDifference(x, y)
-	return BigDec{integral: x.integral - y.integral, scalar: x.scalar}
-}
+// func Sub(x BigDec, y BigDec) BigDec {
+// 	CheckScalarDifference(x, y)
+// 	return BigDec{integral: x.integral - y.integral, scalar: x.scalar}
+// }
 
 func Mul(x BigDec, y BigDec) BigDec {
 	CheckScalarDifference(x, y)
-	return BigDec{integral: (x.integral / x.scalar) * y.integral, scalar: x.scalar}
+	resIntegral := new(big.Int)
+	resIntegral.Mul(x.integral, y.integral)
+	resIntegral.Div(resIntegral, x.scalar)
+	return BigDec{integral: resIntegral, scalar: x.scalar}
 }
 
-func Div(x BigDec, y BigDec) BigDec {
-	CheckScalarDifference(x, y)
-	return BigDec{integral: x.integral / y.integral * x.scalar, scalar: x.scalar}
-}
+// func Div(x BigDec, y BigDec) BigDec {
+// 	CheckScalarDifference(x, y)
+// 	return BigDec{integral: x.integral / y.integral * x.scalar, scalar: x.scalar}
+// }
 
-func Mod(x BigDec, y BigDec) BigDec {
-	CheckScalarDifference(x, y)
-	return BigDec{integral: (x.integral / x.scalar) % (y.integral / y.scalar) * x.scalar, scalar: x.scalar}
-}
+// func Mod(x BigDec, y BigDec) BigDec {
+// 	CheckScalarDifference(x, y)
+// 	return BigDec{integral: (x.integral / x.scalar) % (y.integral / y.scalar) * x.scalar, scalar: x.scalar}
+// }
 
 func RandBoolBigDec() BigDec {
-	return BigDec{integral: int64(rand.Intn(MAX_RAND)) * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
+	// return BigDec{integral: int64(rand.Intn(MAX_RAND)) * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
+	return BigDec{integral: big.NewInt(rand.Int63n(MAX_RAND)), scalar: big.NewInt(DEFAULT_SCALAR)}
 }
 
-func One() BigDec {
-	return BigDec{integral: 1 * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
-}
+// func One() BigDec {
+// 	return BigDec{integral: 1 * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
+// }
 
-func Zero() BigDec {
-	return BigDec{integral: 0 * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
-}
+// func Zero() BigDec {
+// 	return BigDec{integral: 0 * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
+// }
 
 func IntToBigDecDefaultScalar(x int) BigDec {
-	return BigDec{integral: int64(x) * DEFAULT_SCALAR, scalar: DEFAULT_SCALAR}
+	scalar := big.NewInt(DEFAULT_SCALAR)
+	integral := big.NewInt(int64(x))
+	integral.Mul(integral, scalar)
+	return BigDec{integral: integral, scalar: scalar}
 }
 
 func FloatToBigDec(xf float64, scalar int) BigDec {
-	x := int(xf * float64(scalar))
-	return BigDec{int64(x), int64(scalar)}
+	xfBig := big.NewFloat(xf * float64(scalar))
+	x := new(big.Int)
+	xfBig.Int(x)
+	return BigDec{integral: x, scalar: big.NewInt(int64(scalar))}
 }
 
 func (x *BigDec) ToFloat() float64 {
-	return float64(float64(x.integral) / float64(x.scalar))
+	// return float64(float64(x.integral) / float64(x.scalar))
+	integralFloat, _ := x.integral.Float64()
+	scalarFloat, _ := x.scalar.Float64()
+	return integralFloat / scalarFloat
 }
 
-func (x *BigDec) GetScaledInt() int {
-	return int(x.integral / x.scalar)
-}
+// TODO: change this function to return big.Int
+// func (x *BigDec) GetScaledInt() int {
+// 	// return int(x.integral / x.scalar)
+// 	res := new(big.Int)
+// 	res.Div(x.integral, x.scalar)
+// 	return int(res.Int64())
+// }
 
 func FloatArrayToBigDec(array []float64, scalar int) []BigDec {
 	res := make([]BigDec, len(array))
